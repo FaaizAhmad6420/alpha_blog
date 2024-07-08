5177e985ab51b840d439dd7b7c0e3ea0e38f2121
Faaiz Ahmad <faizahmad6420@gmail.com>
2024-07-08 17:41:48 +0500

    Implement CRUD operation and Validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,46 @@ class ArticlesController < ApplicationController
   end
 
   def index
-
+    @articles = Article.all
   end
-  
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was Created Successfully."
+      redirect_to @article
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was Updated Successfully."
+      redirect_to @article
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] = "Article was Deleted Successfully."
+      redirect_to articles_path
+    else
+      flash[:error] = "Error deleting article: #{@article.errors.full_messages.join(', ')}"
+      redirect_to articles_path
+    end
+  end
+
 end
