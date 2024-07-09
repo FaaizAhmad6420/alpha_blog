b957a8ef27310744a8e2e6218c61c95c0696ae36
Faaiz Ahmad <faizahmad6420@gmail.com>
2024-07-09 12:14:39 +0500

    Refactor code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was Created Successfully."
       redirect_to @article
@@ -27,8 +26,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was Updated Successfully."
       redirect_to @article
     else
@@ -37,7 +35,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:notice] = "Article was Deleted Successfully."
       redirect_to articles_path
@@ -45,6 +42,16 @@ class ArticlesController < ApplicationController
       flash[:error] = "Error deleting article: #{@article.errors.full_messages.join(', ')}"
       redirect_to articles_path
     end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
